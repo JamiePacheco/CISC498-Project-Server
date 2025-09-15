525e9aa8830a6e8ce9974c2e2e33fbf3a026ae95
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2025-09-15 10:02:04 -0400

    Initalized spring boot application for server

---
+++ src/main/java/com/aux_arena/AuxArenaApplication.java
@@ -0,0 +1,13 @@
+package com.aux_arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuxArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuxArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aux_arena/AuxArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aux_arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuxArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
