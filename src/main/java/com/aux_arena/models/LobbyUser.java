d11fc1ec77bc9fa1fd10f9fa5a83c50713a13704
Jamie Pacheco <jamie.pacheco.71@gmail.com>
2025-10-20 13:04:51 -0400

    added fields to lobby user model

--- src/main/java/com/aux_arena/models/LobbyUser.java
+++ src/main/java/com/aux_arena/models/LobbyUser.java
@@ -1,15 +1,34 @@
 package com.aux_arena.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@
 public class LobbyUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "game_lobby")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private GameLobby gameLobby;
+
+    @Column(name = "user")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(name = "nickname", length = 100)
+    private String nickname;
+
+    @Column(name = "joined_at")
+    private Instant joinedAt;
+    
 }
